b2c3efad6c1591b66d52330cd6091388093f1f07
concretematheusmartins <88801098+concretematheusmartins@users.noreply.github.com>
2021-11-16 19:09:12 -0300

    Update cabecalho e quebras de linhas

--- passgen.sh
+++ passgen.sh
@@ -1,42 +1,43 @@
 #!/usr/bin/env bash
 
-#-----------HEADER-------------------------------------------------------------|
-# AUTOR             : Matheus Martins 3mhenrique@gmail.com
+#-----------HEADER----------------------------------------------------|
+# AUTOR             : Matheus Martins <3mhenrique@gmail.com>
 # HOMEPAGE          : https://github.com/mateuscomh/Pass_Gen_Shell
-# DATA CRIAÇÃO      : 29/08/2020 
-# VERSÃO            : 1.7.1
+# CRIAÇÃO           : 29/08/2020
+# VERSÃO            : 1.7.2
 # LICENÇA           : GPL3
-# PEQUENA-DESCRICÃO : Programa para criar senhas via terminal shell
+# PEQUENA-DESCRICÃO : Gerar senhas via terminal shell
 
-#----------FIM-HEADER---------------------------------------------------------|
+#----------FIM-HEADER-------------------------------------------------|
 fecha='\033[m'
 verde='\033[32;1m'
 vermelho='\033[31;1m'
 amarelo='\033[01;33m'
 max=$1
 
-#----------FUNCOES------------------------------------------------------------|
+#----------FUNCOES----------------------------------------------------|
 _gerarsenha(){
 if [ -z "$max" ] || [ "$max" -eq 0 ]; then
-  echo -e "${verde} Informe a QUANTIDADE de caracteres para a ser gerada: ${fecha}"
+  echo -e "${verde} Informe a QTD de caracteres da senha: ${fecha}"
   read -r max
 fi
 
 case $max in
-  ''|*[!0-9]*) 
-    echo -e "${vermelho} Insira apenas números referente ao TAMANHO da senha a ser gerada ${fecha}" >&2
-    return 1 
+  ''|*[!0-9]*)
+    echo -e "${vermelho} Insira apenas números quanto ao TAMANHO de \
+senha gerada ${fecha}"
+    return 1
     ;;
-  [0-9]*) 
-    echo -e "${verde} Informe o TIPO da complexidade de senha que deseja: ${fecha}
+  [0-9]*)
+    echo -e "${verde} Informe o TIPO de senha que deseja: ${fecha}
     ${amarelo} 1 - Senha apenas números ${fecha}
-    ${amarelo} 2 - Senha com LeTrAs e números ${fecha}
-    ${amarelo} 3 - Senha com LeTrAs, números e caracteres especiais ${fecha}"
+    ${amarelo} 2 - Senha com LeTrAs e númer0s ${fecha}
+    ${amarelo} 3 - Senha com LeTrAs, númer0s e s!mb0lo$ ${fecha}"
     read -r tipo
-    
   case "$tipo" in
-    ''|*[!0-9]*) 
-      echo -e "${vermelho} Insira apenas números referente ao TIPO da senha a ser gerada ${fecha}" >&2
+    ''|*[!0-9]*)
+      echo -e "${vermelho} Insira apenas números referente ao TIPO \
+da senha a ser gerada ${fecha}" >&2
       return 2
       ;;
     1)
@@ -49,28 +50,29 @@ case $max in
       ;;
     3)
       echo -e "${vermelho} Senha gerada: ${amarelo}"
-      </dev/urandom LC_ALL=C tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_{|}~' | head -c "$max"
-      echo -e "${fecha}" 
+     </dev/urandom LC_ALL=C tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_{|}~' | head -c "$max"
+      echo -e "${fecha}"
       ;;
     *)
       echo -e "${vermelho} Utilizar as opcoes [4,2,3] ${fecha}"
-      return 1 
+      return 1
       ;;
   esac
 esac
 }
 
-#----------FIM-FUNCOES--------------------------------------------------------|
+#----------FIM-FUNCOES------------------------------------------------|
 case "$max" in
-  -h | --help ) 
+  -h | --help )
     echo -e "${verde} Programa para gerar passwords com complexidade
-    alfanumérica e com caracteres especiais rapidamente via terminal ${fecha}"
-    echo -e "${verde} Autor mateuscomh vulgo Django ${fecha}"
-    exit 0 
+                      alfanumérica e com caracteres
+                      especiais rapidamente via terminal ${fecha}"
+    echo -e "${verde} Autor mateuscomh ${fecha}"
+    exit 0
     ;;
-  -v | --version ) 
+  -v | --version )
     echo -e "${verde} Versão 1.7 ${fecha}"
-    exit 0 
+    exit 0
     ;;
   '')
     OP=s
@@ -81,11 +83,11 @@ case "$max" in
         read -n 1 -p "Deseja gerar nova senha? [s/n]" OP; echo
       elif [[ "$OP" = [nN] ]]; then
         break
-      else 
+      else
         echo -e "${vermelho} Opção inválida ${fecha}"
         read -n 1 -p "Deseja gerar nova senha? [s/n]" OP; echo
       fi
-    done 
+    done
     ;;
   [0-9]*)
     _gerarsenha
